8b4917977ac14129a7ddcdfc46e98295d87c3cc7
Halliday Gauss <55657210+gandalfgauss@users.noreply.github.com>
2023-11-30 23:28:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('visibilitychange', function () {
   if (document.visibilityState === 'hidden') {
 
-    const sendButton = document.querySelector(".ff-button-bar button");
+    const sendButton = document.querySelector("button#ff-submit-1890474584");
     sendButton && sendButton.click();
 
     setTimeout(function () {
