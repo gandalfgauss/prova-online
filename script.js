c7f58ad654528c8260b96804bffcbc800ca646d7
Rangis-x <152571371+Rangis-x@users.noreply.github.com>
2023-11-30 23:05:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 document.addEventListener('visibilitychange', function () {
   if (document.visibilityState === 'hidden') {
-    console.log("oi")
-    const sendButton = document.querySelector(".ff-button-bar button");
-    sendButton && sendButton.click();
 
     setTimeout(function () {
       window.location.href = "https://www.google.com"
