63302cfaa26e3712d9677ace3594459ef4fce3f8
halliday.santos <halliday.santos@petrosoftdesign.com>
2023-12-03 19:59:34 -0300

    Ajustando links para formularios reais

--- prova-online/src/InformacoesTurmas/InformacoesTurmas.js
+++ prova-online/src/InformacoesTurmas/InformacoesTurmas.js
@@ -1,21 +1,27 @@
 
 const InformacoesTurmas = [
   {
-    nome: "Turma 1",
-    rotaUrl: "turma1",
-    linkFormulario: "https://formfacade.com/include/114005282582888746142/form/1FAIpQLSd_j-bczDt5K_jQDFNyJ4VcIXoCAwpKP6FpJ5b_vM6MpYTH0Q/classic.js?div=ff-compose"
+    nome: "1R1",
+    rotaUrl: "1R1",
+    linkFormulario: "https://formfacade.com/include/110324158331590689605/form/1FAIpQLSfKbipNt_XTooiy27Bi3QAvy8gFAg-Eizo1XQluxi0CLYSjng/classic.js?div=ff-compose"
   },
 
   {
-    nome: "Turma 2",
-    rotaUrl: "turma2",
-    linkFormulario: "https://formfacade.com/include/114005282582888746142/form/1FAIpQLSdpJQ2idR1HHlS5WsRVI4Y9Em4edauKbDZgLJtAPzlHcXKjBg/classic.js?div=ff-compose"
+    nome: "1R2",
+    rotaUrl: "1R2",
+    linkFormulario: "https://formfacade.com/include/110324158331590689605/form/1FAIpQLSevgiBwqLSsRtkSm6WacpHN84GHZYFLhzt0GGPXqdAHgH2ilA/classic.js?div=ff-compose"
   },
 
   {
-    nome: "Turma 3",
-    rotaUrl: "turma3",
-    linkFormulario: "https://formfacade.com/include/114005282582888746142/form/1FAIpQLSdmLvGdP2N0733h1m-kkaNAaLQgSdm_PtoNXVoDZ0HXQ18HhA/classic.js?div=ff-compose"
+    nome: "2R2",
+    rotaUrl: "2R2",
+    linkFormulario: "https://formfacade.com/include/110324158331590689605/form/1FAIpQLSdHAW_ni3Vp9BeugtytSNqWWQbYBhSp9sY6w2gcsnzPf5wEzA/classic.js?div=ff-compose"
+  },
+
+  {
+    nome: "2R3",
+    rotaUrl: "2R3",
+    linkFormulario: "https://formfacade.com/include/110324158331590689605/form/1FAIpQLSffFrrkiclxhRdHZuAz3_QB1r-dj6dQFY3izL5y52lUQABXrg/classic.js?div=ff-compose"
   }
 ];
 
